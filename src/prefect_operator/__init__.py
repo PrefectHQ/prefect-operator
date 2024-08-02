5165b5dbaa203fae1f2ca37f715f6719cec91838
Chris Guidry <chris.g@prefect.io>
2024-08-02 12:55:12 -0400

    Back to virtual versioning

--- src/prefect_operator/__init__.py
+++ src/prefect_operator/__init__.py
@@ -1,3 +1,5 @@
-from prefect_operator._version import __version__  # noqa: F401
+from importlib.metadata import version
+
+__version__ = version("prefect-operator")
 
 DEFAULT_PREFECT_VERSION = "3.0.0rc13"
