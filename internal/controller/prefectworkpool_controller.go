404e0df9130b72c0bf54840cf1ea9a4c6942b7c4
Mitchell Nielsen <mitchnielsen@users.noreply.github.com>
2024-08-29 16:48:56 -0500

    Use CreateOrUpdate for WorkPool Deployment (#57)
    Follow up to https://github.com/PrefectHQ/prefect-operator/pull/55
    for WorkPools.

--- internal/controller/prefectworkpool_controller.go
+++ internal/controller/prefectworkpool_controller.go
@@ -18,22 +18,23 @@ package controller
 
 import (
 	"context"
-	"fmt"
 
+	"dario.cat/mergo"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
-	utilerrors "k8s.io/apimachinery/pkg/util/errors"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	ctrllog "sigs.k8s.io/controller-runtime/pkg/log"
 
 	prefectiov1 "github.com/PrefectHQ/prefect-operator/api/v1"
 	"github.com/PrefectHQ/prefect-operator/internal/conditions"
+	"github.com/PrefectHQ/prefect-operator/internal/constants"
+	"github.com/PrefectHQ/prefect-operator/internal/status"
 )
 
 // PrefectWorkPoolReconciler reconciles a PrefectWorkPool object
@@ -52,81 +53,63 @@ type PrefectWorkPoolReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.17.3/pkg/reconcile
 func (r *PrefectWorkPoolReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	var condition metav1.Condition
-	var err error
-
 	log := ctrllog.FromContext(ctx)
 
 	log.Info("Reconciling PrefectWorkPool")
 
 	workPool := &prefectiov1.PrefectWorkPool{}
-	err = r.Get(ctx, req.NamespacedName, workPool)
+	err := r.Get(ctx, req.NamespacedName, workPool)
 	if errors.IsNotFound(err) {
 		return ctrl.Result{}, nil
 	} else if err != nil {
 		return ctrl.Result{}, err
 	}
 
+	// Defer a final status update at the end of the reconciliation loop, so that any of the
+	// individual reconciliation functions can update the status as they see fit.
 	defer func() {
-		if updateErr := r.updateCondition(ctx, workPool, condition); updateErr != nil {
-			err = utilerrors.NewAggregate([]error{updateErr, err})
+		if statusErr := r.Status().Update(ctx, workPool); statusErr != nil {
+			log.Error(statusErr, "Failed to update WorkPool status")
 		}
 	}()
 
+	objName := constants.Deployment
+
 	desiredDeployment := r.prefectWorkerDeployment(workPool)
 
-	workPoolNamespacedName := types.NamespacedName{
-		Namespace: workPool.Namespace,
-		Name:      workPool.Name,
+	deploy := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      desiredDeployment.Name,
+			Namespace: desiredDeployment.Namespace,
+		},
 	}
 
-	foundDeployment := &appsv1.Deployment{}
-	err = r.Get(ctx, workPoolNamespacedName, foundDeployment)
-	if errors.IsNotFound(err) {
-		log.Info("Creating Deployment", "name", desiredDeployment.Name)
-		if err = r.Create(ctx, &desiredDeployment); err != nil {
-			condition = conditions.NotCreated("Deployment", err)
-			return ctrl.Result{}, err
+	result, err := controllerutil.CreateOrUpdate(ctx, r.Client, deploy, func() error {
+		if err := ctrl.SetControllerReference(workPool, deploy, r.Scheme); err != nil {
+			return err
 		}
-		condition = conditions.Created("Deployment")
-	} else if err != nil {
-		condition = conditions.UnknownError("Deployment", err)
+
+		return mergo.Merge(deploy, desiredDeployment, mergo.WithOverride)
+	})
+
+	log.Info("CreateOrUpdate", "object", objName, "name", desiredDeployment.Name, "result", result)
+
+	if err != nil {
+		meta.SetStatusCondition(&workPool.Status.Conditions, conditions.UnknownError(objName, err))
 		return ctrl.Result{}, err
-	} else if !metav1.IsControlledBy(foundDeployment, workPool) {
-		errorMessage := fmt.Sprintf(
-			"%s %s already exists and is not controlled by PrefectWorkPool %s",
-			"Deployment", desiredDeployment.Name, workPool.Name,
-		)
-		condition = conditions.AlreadyExists("Deployment", errorMessage)
-		return ctrl.Result{}, errors.NewBadRequest(errorMessage)
-	} else if deploymentNeedsUpdate(&foundDeployment.Spec, &desiredDeployment.Spec, log) {
-		log.Info("Updating Deployment", "name", desiredDeployment.Name)
+	}
 
-		condition = conditions.NeedsUpdate("Deployment")
+	meta.SetStatusCondition(&workPool.Status.Conditions, status.GetStatusConditionForOperationResult(result, objName, err))
 
-		if err = r.Update(ctx, &desiredDeployment); err != nil {
-			condition = conditions.UpdateFailed("Deployment", err)
-			return ctrl.Result{}, err
-		}
-
-		condition = conditions.Updated("Deployment")
-	} else {
-		if !meta.IsStatusConditionTrue(workPool.Status.Conditions, "DeploymentReconciled") {
-			condition = conditions.Updated("Deployment")
-		}
-
-		imageVersion := prefectiov1.VersionFromImage(foundDeployment.Spec.Template.Spec.Containers[0].Image)
-		readyWorkers := foundDeployment.Status.ReadyReplicas
+	if result == controllerutil.OperationResultUpdated {
+		imageVersion := prefectiov1.VersionFromImage(deploy.Spec.Template.Spec.Containers[0].Image)
+		readyWorkers := deploy.Status.ReadyReplicas
 		ready := readyWorkers > 0
 
 		if workPool.Status.Version != imageVersion || workPool.Status.ReadyWorkers != readyWorkers || workPool.Status.Ready != ready {
 			workPool.Status.Version = imageVersion
 			workPool.Status.ReadyWorkers = readyWorkers
 			workPool.Status.Ready = ready
-
-			if statusErr := r.Status().Update(ctx, workPool); statusErr != nil {
-				return ctrl.Result{}, statusErr
-			}
 		}
 	}
 
